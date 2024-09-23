8ac107e65e4778a9f62048f37523ba51127bb6ad
emmanuel <emifatusin@gmail.com>
2024-09-22 23:40:23 -0500

    Updated name and card length

--- 1015-WWCCV.py
+++ 1015-WWCCV.py
@@ -14,7 +14,9 @@ card_number = input("Enter your 16-digit credit card number: ")
 # len means 'length', and the '==' is testing one thing against the other. A single equal sign would set one thing equal to the other (not what we want to do). 
 # card_number.isdigit() is checking if this is numbers instead of someone typing 'cheeseburger' for their credit card number.
 
-if len(card_number) == 16 and card_number.isdigit():     
-    print ("Card is valid.")
+card_number = str(card_number).strip()
+
+if len(card_number) == 16 and card_number.isdigit():
+    print("Card is valid.")
 else:
-    print ("Invalid card number. It must be 16 digits long.") 
+    print("Invalid card number. It must be 16 digits long.")
